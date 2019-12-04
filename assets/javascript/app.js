34edb27948503dab5c74b3fc30bdb87d45324caa
Pilot <trevorjburni@gmail.com>
2019-12-04 08:46:48 -0700

    added the table to display schedule and form to add another train to schedule.

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBrmy9qvD6evSEYi6w_Esdm2C5Kppv6cf8",
+    authDomain: "train-departures-a6ed4.firebaseapp.com",
+    databaseURL: "https://train-departures-a6ed4.firebaseio.com",
+    projectId: "train-departures-a6ed4",
+    storageBucket: "train-departures-a6ed4.appspot.com",
+    messagingSenderId: "428613739560",
+    appId: "1:428613739560:web:42ef56519d6e3a3c14c11d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
